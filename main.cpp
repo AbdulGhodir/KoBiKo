8dbe25ec7e0e86cd5ac4f8d922e2838732aea571
elisatisya <elisatisya1@gmail.com>
2024-12-09 14:31:12 +0700

    desimal convert to all

--- main.cpp
+++ main.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
 #include "function/define.h"
-
+using namespace std;
 int main() {
     int pilihan;
-    std::cin >> pilihan;
-    desimalToOktal(pilihan);
+    cout << "Masukkan pilihan konverter: " << endl;
+    cout << "1. Desimal ke Oktal\n";
+    cout << "2. Desimal ke Biner\n";
+    cout << "3. Desimal ke Heksadesimal\n";
+    cout << "0. Keluar\n";
+    cin >> pilihan;
+
+
+    
+    while (pilihan != 0)
+    {
+        switch (pilihan)
+        {
+        case 1:
+            desimalToOktal();
+            break;
+        case 2:
+            desimalToBiner();
+            break;
+        case 3:
+            desimalToHexa();
+            break;
+        break;
+    }
+    }
 }
