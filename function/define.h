bed67b2fd7393be9950d0a20c8d25adcf2398876
M. Rafly Saputra <966raflisaputra@gmail.com>
2024-12-09 13:50:57 +0700

    inisialisasi folder

---
+++ function/define.h
@@ -0,0 +1,17 @@
+#ifndef KOVERTER
+#define KOVERTER
+
+int desimalToOktal(int desimal);
+int desimalToBiner(int desimal);
+int desimalToHexa(int desimal);
+int binerToDesimal(int desimal);
+int binerToOktal(int desimal);
+int binerToHexa(int desimal);
+int oktalToDesimal(int desimal);
+int oktalToBiner(int desimal);
+int oktalToHexa(int desimal);
+int hexaToDesimal(int desimal);
+int hexaToBiner(int desimal);
+int hexaToOktal(int desimal);
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "function/define.h"
+
+int main() {
+    int pilihan;
+    std::cin >> pilihan;
+    desimalToOktal(pilihan);
+}
